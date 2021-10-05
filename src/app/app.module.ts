198903a49c0fb7dbaeeefb28d46546562076115e
viniyr <vinicius.yonezawa@gmail.com>
2021-10-05 19:47:00 -0300

    Saving logged users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoryService } from '../services/domain/category.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storatge.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { AuthService } from '../services/auth.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoryService,
     ErrorInterceptorProvider,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@angular/core";
 import { CredentialsDTO } from "../models/credentials.dto";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storatge.service";
 
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http: HttpClient) {
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
 
     authenticate(creds : CredentialsDTO) {
@@ -18,5 +20,17 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
+    }
+
+    sucessfulLogin(authorizationValue : string) { 
+        let tok = authorizationValue.substring(7);
+        let user : LocalUser = { 
+            token : tok
+        };
+        this.storage.setLocalUser(user);
+    }
+
+    logout() { 
+        this.storage.setLocalUser(null);
     }
 }
