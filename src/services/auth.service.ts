f79b57eeafbc2ee354c59bb93d8f0e929d73a3f3
viniyr <vinicius.yonezawa@gmail.com>
2021-10-05 20:02:14 -0300

    Extracting email from token

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,10 +4,13 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storatge.service";
+import { JwtHelper } from "angular2-jwt";
 
 
 @Injectable()
 export class AuthService {
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient, public storage: StorageService) {
     }
@@ -25,7 +28,8 @@ export class AuthService {
     sucessfulLogin(authorizationValue : string) { 
         let tok = authorizationValue.substring(7);
         let user : LocalUser = { 
-            token : tok
+            token : tok,
+            email : this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }
